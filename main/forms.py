15a38568dcc33f2fbab6e0dc6c1517d66bdcbce8
Dmitry Parshukov <dimer3799@yandex.ru>
2020-06-14 16:10:07 +0600

    fix registeration users

--- main/forms.py
+++ main/forms.py
@@ -17,8 +17,10 @@ class ChangeUserInfoForm(forms.ModelForm):
 
 class RegisterUserForm(forms.ModelForm):
     email = forms.EmailField(required=True, label='Адрес электронной почты')
-    password1 = forms.CharField(label='Пароль', widget = forms.PasswordInput, help_text=password_validation.password_validators_help_text_html())
-    password2 = forms.CharField(label='Пароль (повторно)', widget = forms.PasswordInput, help_text='Введите тот же самый пароль еще раз для проверки')
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput,
+                help_text=password_validation.password_validators_help_text_html())
+    password2 = forms.CharField(label='Пароль (повторно)', widget=forms.PasswordInput,
+                help_text='Введите тот же самый пароль еще раз для проверки')
 
     def clean_password1(self):
         password1 = self.cleaned_data['password1']
@@ -31,22 +33,24 @@ class RegisterUserForm(forms.ModelForm):
         password1 = self.cleaned_data['password1']
         password2 = self.cleaned_data['password2']
         if password1 and password2 and password1 != password2:
-            errors = {'password1':ValidationError('Введенные пароли не совпадают', code = 'password_mismatch')}
+            errors = {'password2': ValidationError('Введенные пароли не совпадают',
+                                  code='password_mismatch')}
             raise ValidationError(errors)
 
-    def save(self, commit = True):
-        user = super().save(commit = False)
+    def save(self, commit=True):
+        user = super().save(commit=False)
         user.set_password(self.cleaned_data['password1'])
         user.is_active = False
         user.is_activated = False
         if commit:
             user.save()
-        user_registrated.send(RegisterUserForm, isnstance = user)
+        user_registrated.send(RegisterUserForm, instance=user)
         return user
 
     class Meta:
         model = AdvUser
-        fields = ('username', 'email', 'first_name', 'last_name', 'send_messages')
+        fields = ('username', 'email', 'password1', 'password2', 'first_name', 'last_name',
+                  'send_messages')
 
 
 
--- main/urls.py
+++ main/urls.py
@@ -6,12 +6,12 @@ urlpatterns = [
 	path('accounts/register/activate/<str:sign>/', user_activate, name = 'register_activate'),
 	path('accounts/register/done/', RegisterDoneView.as_view(), name = 'register_done'),
 	path('account/register/', RegisterUserView.as_view(), name = 'register'),
-	path('<str:page>/', other_page, name='other'),
-	path('',index, name='index'),
+	path('accounts/login/', BBLoginView.as_view(), name = 'login'),	
 	path('accounts/password/change/',BBPaswwordChangeViews.as_view(), name = 'password_change'),
-	path('accounts/profile/change/', ChangeUserInfoView.as_view(), name = 'profile_change'),
-	path('accounts/login/', BBLoginView.as_view(), name = 'login'),
+	path('accounts/profile/change/', ChangeUserInfoView.as_view(), name = 'profile_change'),	
 	path('accounts/profile/', profile, name = 'profile'),
 	path('accounts/logout/', BBLogoutView.as_view(), name = 'logout'),
+	path('<str:page>/', other_page, name='other'),
+	path('',index, name='index'),
 
 ]
